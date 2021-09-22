f15a3bba482410c1946bd07395dbf56820021cb5
Arian <adilsonarian@outlook.com>
2021-09-21 20:09:12 -0600

    Interfaz Principal completa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,8 @@ namespace Ejercicio_4___Guía3_MM200149
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -144,11 +146,20 @@ namespace Ejercicio_4___Guía3_MM200149
             this.button3.Text = "Eliminar";
             this.button3.UseVisualStyleBackColor = false;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(490, 188);
+            this.dataGridView1.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 307);
+            this.ClientSize = new System.Drawing.Size(576, 443);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox4);
@@ -162,6 +173,7 @@ namespace Ejercicio_4___Guía3_MM200149
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Ejercicio 4 Guía 3 MM200149";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,6 +192,7 @@ namespace Ejercicio_4___Guía3_MM200149
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
